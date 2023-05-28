9f3ce003ef72870f7999506d05d4675a95442fdf
JR-Vickers <jarrett_vickers@yahoo.com>
2023-05-28 08:29:07 -0500

    chapter14

---
+++ chapter14/quiz.py
@@ -0,0 +1,16 @@
+"""
+1.  How are for loops and iterable objects related?
+    All for loops uses the iteration protocol to step through items in the iterable object.
+
+2.  How are for loops and list comprehensions related?
+    Both are iteration tools and contexts.
+
+3.  Name four iteration contexts in the Python language.
+    for loop
+    list comprehensions
+    map function
+    functions sorted, sum, any, and all
+
+4.  What is the best way to read line by line from a text file today?
+    Don't read it at all - open the file within an iteration context tool and let it automatically scan one line at a time.
+"""
