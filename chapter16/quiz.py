28ae99af72fc5a9d697755e7f8861f33f832526b
JR-Vickers <jarrett_vickers@yahoo.com>
2023-05-30 07:11:41 -0500

    chapter16

---
+++ chapter16/quiz.py
@@ -0,0 +1,16 @@
+"""
+1.  What is the point of coding functions?
+    They allow you to reuse code without copying and pasting.
+
+2.  At what time does Python create a function?
+    At runtime.
+
+3.  What does a function return if it has no return statement in it?
+    A None statement, though this statement is usually ignored.
+
+4.  When does the code nested inside the function definition statement run?
+    When the function is called.
+
+5.  What's wrong with checking the types of objects passed into a function?
+    It breaks the function's flexibility.
+"""
