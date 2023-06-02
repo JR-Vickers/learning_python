f8ebe1ce39cc7cddb06679a7e7c8bc853cfb466e
JR-Vickers <jarrett_vickers@yahoo.com>
2023-06-02 10:32:10 -0500

    chapter19

---
+++ chapter19/quiz.py
@@ -0,0 +1,27 @@
+"""
+1.  How are lambda expressions and def statements related?
+    They both create function objects.
+
+2.  What's the point of using lambda?
+    It's an unnamed, or anonymous function.  It's typically used for inline functions.
+
+3.  Compare and contast map, filter, and reduce.
+    Map allows you to apply an operation to every item lists or other sequences.
+    Filter takes a list and returns a list of all items that pass through a conditional statement.
+    Reduce returns the result of the iterables passed through a lambda function.
+
+4.  What are function annotations, and how are they used?
+    They are simply additional information attached to functions for use by other tools.
+
+5.  What are recursive functions, and how are they used?
+    A recursive function calls itself in order to repeat a given section of code.
+    One major use case of recursive functions is to traverse arbitrarily-shaped data structures.
+
+6.  What are some general design guidelines for coding functions?
+    They should be relatively short - less than 1 page.
+
+7.  Name three or more ways that functions can communicate results to a caller.
+    return statement
+    changing passed-in mutable arguments
+    setting global variables
+"""
